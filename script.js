4ae5274472beb25224f2794507b574fa3df41782
Ahla <ahlausman139@gmail.com>
2025-10-17 18:59:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-            /* Individual task objects*/
-            const task1 = { id: 1, name: "Buy groceries", completed: false };
-            const task2 = { id: 2, name: "Pray Salah",completed: false };
-            const task3 = { id: 3, name: "Do Dhikr",completed: true };
-            const task4 = { id: 4, name: "Do homework",completed: false };
-            const task5 = { id: 5, name: "watch podcasts", completed: true };
+ 
+    const input = document.getElementById('addTask');
+    const taskContainer = document.getElementById('taskContainer');
 
-            const tasks=[task1, task2, task3, task4, task5];
-            
-             /* store task objects in array*/
-        for ( let i=0; i<tasks.length; i++){
-            let task = tasks[i];
+    addTask.addEventListener('click', function() {
+      
+      const newInput = document.createElement('input');
+      newInput.type = 'text';
+      newInput.placeholder = 'Add your task';
 
-            if (task.completed){
-                console.log( `Task ${task.name} is completed!`)
-            } else {
-                console.log(`Task  ${ task.name } is still pending.`)
-            }
-        }
+    
+      taskContainer.appendChild(newInput);
+    });
