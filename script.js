2eb1782ab9f07744ab3f6772f9670aa35ee381f2
Ahla <ahlausman139@gmail.com>
2025-10-23 11:41:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
- 
-    const input = document.getElementById('addTask');
-    const taskContainer = document.getElementById('taskContainer');
+const inputBox= document.getElementById('input-box');
+const listContainer= document.getElementById('list-container');
+function addTask(event) {
+    event.preventDefault();
 
-    addTask.addEventListener('click', function() {
-      
-      const newInput = document.createElement('input');
-      newInput.type = 'text';
-      newInput.placeholder = 'Add your task';
+    if (inputBox.value === '') {
+        alert('You must add your task');
+    } else {
+        let li = document.createElement("li");
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className = "task-checkbox";
+
+        let taskText = document.createElement("span");
+        taskText.className = "task-text";
+        taskText.innerText = inputBox.value;
+
+        
+        let deleteBtn = document.createElement("span");
+        deleteBtn.innerHTML = "\u00d7";
+        deleteBtn.className = "delete-btn";
 
     
-      taskContainer.appendChild(newInput);
-    });
+        li.appendChild(checkbox);
+        li.appendChild(taskText);
+        li.appendChild(deleteBtn);
+        listContainer.appendChild(li);
+    }
+
+    inputBox.value = "";
+}
+
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName=== "LI"){
+        e.target.classList.toggle("checked");
+    }else if (e.target.tagName=== "SPAN"){
+        e.target.parentElement.remove();
+    }
+} , false); 
